de9d7186db0f60a6c3d77df281f3db8f41269f12
3djon <3djoncaro@gmail.com>
2025-01-21 18:05:39 -0500

    Creating word count function
    Printing the total word count at prgram run

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 def main():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-main()
+        file_contents = f.read() # reading the contents of above file
+        word_count = count_words(file_contents) # calling word count function
+        print(f"Word count: {word_count}") # This displays the full book text to terminal
+
+if __name__ == "__main__":
+    main()
