87f8a9442b09f442d10731b49950e241bf1092ab
3djon <3djoncaro@gmail.com>
2025-01-21 22:41:41 -0500

    Created sort_report function which cleanly displays word count and individual unique alphabet character counts
    Cleaned up main to only read contents of provided text file and call sort_report

--- main.py
+++ main.py
@@ -7,21 +7,45 @@ def count_characters(text):
     text = text.lower() # converts all text to lowercase
 
     for character in text:
-        if character in string_count:
-            string_count[character] += 1 # add one to its count
-        else:
-            string_count[character] = 1 # start the count at 1
-    # Create a sorted version for display
-    sorted_chars = dict(sorted(string_count.items()))
-    return sorted_chars
+        if character.isalpha(): # only count of its a letter
+            if character in string_count:
+                string_count[character] += 1 # add one to its count
+            else:
+                string_count[character] = 1 # start the count at 1
+    return string_count
+
+def sort_report(text):
+    # Retrieve word counds from function
+    word_count = count_words(text)
+    # Retrieve character counts from function
+    char_counts = count_characters(text)
+
+    # Convert dict to a LIST of dictionaries
+    char_list = []
+    for char, count in char_counts.items():
+        # Create a new dictionary for this character
+        char_dict = {'char': char, 'count': count}
+        # Add it to our list
+        char_list.append(char_dict)
+ 
+    # Defining the helper function
+    def sort_on(dict):
+        return dict["count"]
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    # Creating the organized report
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+    for char_dict in char_list:
+        print(f"The '{char_dict['char']}' character was found {char_dict['count']} times")
+    print("--- End report ---")
 
 
 def main():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read() # reading the contents of above file
-        word_count = count_words(file_contents) # calling word count function
-        char_count = count_characters(file_contents)# calling character counting function
-        print(char_count)
-
+        file_contents = f.read() # Reading the contents of text document
+        sort_report(file_contents) # Generates a report of amount of words and unique character count of provided text
+        
 if __name__ == "__main__":
     main()
