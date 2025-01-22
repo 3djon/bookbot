342c704d94bc5ba3b848b1ca2e932c7ad8782138
3djon <3djoncaro@gmail.com>
2025-01-21 21:51:05 -0500

    created character count function. Prints a dictionary that counts all characters in the provided text

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ def count_words(text):
     words = text.split() # adds all individual words into a list
     return len(words) # counts the words in the list
 
+def count_characters(text):
+    string_count = {} # dictionary to store counted letters
+    text = text.lower() # converts all text to lowercase
+
+    for character in text:
+        if character in string_count:
+            string_count[character] += 1 # add one to its count
+        else:
+            string_count[character] = 1 # start the count at 1
+    # Create a sorted version for display
+    sorted_chars = dict(sorted(string_count.items()))
+    return sorted_chars
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read() # reading the contents of above file
         word_count = count_words(file_contents) # calling word count function
-        print(f"Word count: {word_count}") # This displays the full book text to terminal
+        char_count = count_characters(file_contents)# calling character counting function
+        print(char_count)
 
 if __name__ == "__main__":
     main()
